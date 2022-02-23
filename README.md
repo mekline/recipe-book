9b9cde1cc923611bda5564a8faad6d6a308e251e
Melissa Kline Struhl <mekline@mekline.local>
2022-02-23 17:36:55 -0500

    added recipe maker

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # recipe-book
 Compile a recipe book from submissions to a google form!
+
+This code assumes you have a text format (TSV/CSV) version of recipe data called `recipes.tsv`. I created this by having people enter recipes into a google form.  If people follow a request to do things like number each step in a recipe on its own line, you're in business! The key to getting this working well-enough was to separate the ingredients and instructions into two separate boxes. But anyhow, expect to still do manual formatting to clean/match styles once you have compiled everything, and add in the recipes of people who sent you photographs of newspaper clippings :)
+
+The general workflow is to convert the TSV -> html -> word document -> pdf.  The html-> word conversion is performed just by forcing Word to open the html file, and there is probably a better way to create a PDF than that! Oh well!
---
+++ recipemaker.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import re
+
+outstring = ''
+recipes = pd.read_csv('recipes.tsv', sep='\t')
+#print(recipes.head(5))
+
+recipes = recipes.rename(index=str, columns={"Your name": "Name",\
+    "Name of your recipe": "RecipeName",\
+    "Description or story about your recipe": "Story",\
+    "Thank you!": "Notes",\
+    "What kind of recipe is this?":"Rtype"})
+
+recipes['Ordering'] = np.where(recipes["Rtype"] == 'Breakfast', 1, recipes["Rtype"])
+recipes['Ordering'] = np.where(recipes["Rtype"] == 'Appetizer', 2, recipes["Ordering"])
+recipes['Ordering'] = np.where(recipes["Rtype"] == 'Salad', 3, recipes["Ordering"])
+recipes['Ordering'] = np.where(recipes["Rtype"] == 'Side Dish', 3, recipes["Ordering"])
+recipes['Ordering'] = np.where(recipes["Rtype"] == 'Soup', 3, recipes["Ordering"])
+recipes['Ordering'] = np.where(recipes["Rtype"] == 'Main Dish', 4, recipes["Ordering"])
+recipes['Ordering'] = np.where(recipes["Rtype"] == 'Dessert', 5, recipes["Ordering"])
+
+#recipes["Rtype"] = pd.Categorical(recipes["Rtype"], ["Breakfast", "Appetizer", "Soup", "Salad", "Main Dish", "Side Dish", "Dessert"])
+
+recipes = recipes.fillna("")
+
+#print recipes.head(5)
+print recipes["Ordering"]
+
+recipes["FullText"] = '<h1>' + recipes["RecipeName"] + '</h1>' + \
+"<p><i>" + recipes["Story"] + "</i></p>" + \
+"<p>---"+ recipes["Name"] + "</p><br>" + \
+"<h3>Ingredients</h3>" + \
+"<p>"+ recipes["Ingredients"] + "</p><br>" + \
+"<h3>Instructions</h3>" + \
+"<p>"+ recipes["Instructions"] + "</p><br>" + \
+"<p><i>" + recipes["Notes"] + "</i></p><p>*****</p>"
+
+recipes["FullText"] = recipes["FullText"].replace(to_replace=r'([0-9]\))', value=r'</p><p>\1', regex=True)
+
+recipes = recipes.sort("Ordering")
+
+recipes["FullText"].to_csv('recipetext.html', index=False)
+
+with open('recipetext.html') as f:
+	newText=f.read().replace('"\n"', '\n')
+
+with open('recipetext.html', "w") as f:
+  	f.write(newText)
